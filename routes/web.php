39fd391981ec28e35ab9336f8a114b80746a9386
Ivan Sabat <ivan.sabat@outlook.com>
2025-07-06 12:46:03 +0300

    Add project files

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return redirect('/dashboard');
+});
+
+Route::view('/dashboard', 'dashboard')->name('dashboard');
